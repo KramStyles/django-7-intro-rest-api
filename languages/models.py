69a4e7d26190acbd75b93d7e706b8e71b4462a7f
kramstyles <mark.eke@decagon.dev>
2022-04-09 20:39:09 +0100

    added models

--- languages/models.py
+++ languages/models.py
@@ -1,3 +1,6 @@
+from operator import mod
 from django.db import models
 
-# Create your models here.
+class Languages(models.Model):
+    name = models.CharField(max_length=100)
+    paradigm = models.CharField(max_length=100)
