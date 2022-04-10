6bee5a8a0098cc0d87ea393deb08c461446d2037
kramstyles <mark.eke@decagon.dev>
2022-04-10 08:40:57 +0100

    worked on get, post and delete

--- todo_api/views.py
+++ todo_api/views.py
@@ -6,9 +6,22 @@ from .models import Tasks
 from .serializers import TodoSerializer
 
 class TodoList(APIView):
+    def get(self, request):
+        todo = Tasks.objects.all()
+        todo_data = TodoSerializer(todo, many=True)
+        return Response(todo_data.data)
+
     def post(self, request):
-        todo = TodoSerializer(request.data)
+        todo = TodoSerializer(data=request.data)
+        if todo.is_valid(): 
+            item = todo.save()
+            item.finished = False
+            item.save()
         return Response(todo.data)
+
+    def delete(self, request):
+        Tasks.objects.all().delete()
+        return Response(None)
 
 class TodoSingle(APIView):
     pass
