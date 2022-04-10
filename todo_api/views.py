653527622148b7fd44403e62e6d1e5270894d488
kramstyles <mark.eke@decagon.dev>
2022-04-10 08:19:56 +0100

    added serializers.py

--- todo_api/views.py
+++ todo_api/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+from rest_framework.response import Response
 
 from .models import Tasks
+from .serializers import TodoSerializer
 
 class TodoList(APIView):
-    pass
+    def post(self, request):
+        todo = TodoSerializer(request.data)
+        return Response(todo.data)
 
 class TodoSingle(APIView):
     pass
